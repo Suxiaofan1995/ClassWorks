76350d88423b41d156cccdc0a4c4b2f4549036e5
stu <stu@qq.com>
2016-11-07 17:59:56 +0800

    test1107

---
+++ 123/161017/161017/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _161017.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult welcome()
+        {
+            var a = 100;
+            var sum = 0;
+            for (var i = 1; i <= a; i++)
+            {
+                sum += i;
+            }
+
+            ViewBag.sum = sum;
+            string[] data = new string[] {
+                "李克强：奋力开创东北全面振兴新局面","神十一与天宫对接成功 航天员“入宫”","这些作品影响习近平数十年"
+            };
+            ViewBag.data = data;
+            return View();
+           
+        }
+        public ActionResult list()
+        {
+            string[] data = new string[] {
+                "李克强：奋力开创东北全面振兴新局面","神十一与天宫对接成功 航天员“入宫”","这些作品影响习近平数十年"
+            };
+            ViewBag.data = data;
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult add()
+        {
+            return View();
+        }
+        public ActionResult save()
+        {
+            return View();
+        }
+    }
+}
